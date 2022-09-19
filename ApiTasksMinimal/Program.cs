a31f7d6faf091e73535c4725a2c5697841ade170
Gabriel Brassaroto <Gabriel Brassaroto@DESKTOP-BUP64P5>
2022-09-19 20:42:38 -0300

    Create end points crud,model e and class

--- ApiTasksMinimal/Program.cs
+++ ApiTasksMinimal/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TasksDB"));
+
 
 var app = builder.Build();
 
@@ -14,6 +21,73 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/", () => "Olá Mundo");
+app.MapGet("/", () => "Hello World.:) ");
+
+
+app.MapGet("phrases", async async=> await new HttpClient().GetStreamAsync("https://ron-swanson-quotes-herokuapp.com/v2/quotes"));
+
+
+app.MapGet("/tasks", async (AppDbContext db) => { return await db.Tasks.ToListAsync(); });
+
+app.MapGet("/tasks/{id}", async (int id, 
+    AppDbContext db) => await
+  db.Tasks.FindAsync(id) is Task task ? Results.Ok(task): Results.NotFound());
+
+app.MapGet("/tasks/completed", async (AppDbContext db) => 
+    await db.Tasks.Where(x => x.IsCompleted).ToListAsync());
+
+app.MapPost("/tasks", async (Task task, AppDbContext db) =>
+ {
+     db.Tasks.Add(task);
+     await db.SaveChangesAsync();
+     return Results.Created($"/tasks/{task.Id}", task);
+ });
+
+
+
+app.MapPut("/tasks/{id}", async (int id, Task inputTask, AppDbContext db) =>
+{
+    var task = await db.Tasks.FindAsync(id);
+    if (task is null) return Results.NotFound();
+
+    task.Name = inputTask.Name; 
+    task.IsCompleted = inputTask.IsCompleted;   
+    await db.SaveChangesAsync();
+    return Results.NoContent(); 
+});
+
+
+app.MapDelete("/tasks/{id}", async (int id, AppDbContext db) =>
+{
+    if (await db.Tasks.FindAsync(id) is Task task )
+    {
+        db.Tasks.Remove(task);
+        await db.SaveChangesAsync();
+        return Results.Ok(task);    
+    }
+    
+    return Results.NotFound();
+});
+
+
 
 app.Run();
+
+
+class Task
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public bool IsCompleted { get; set; }
+}
+
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {}
+
+    public DbSet<Task> Tasks => Set <Task>();
+}
